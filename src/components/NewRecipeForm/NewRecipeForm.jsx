620be3529fd216cf77169fa23d8c7ce4b614018b
MannySanz <mannysanz715@gmail.com>
2023-04-16 16:06:18 -0700

    Set Add ingredients and displayed ingredients to only show the ones user has added.

--- src/components/NewRecipeForm/NewRecipeForm.jsx
+++ src/components/NewRecipeForm/NewRecipeForm.jsx
@@ -46,37 +46,37 @@ const NewRecipeForm = ({handleChange}) => {
         <div className="checklist">
           <h4>Allergens</h4>
           <input value='Dairy' name='allergens' onChange={handleChange} id='diary' type='checkbox'/>
-          <label for='diary'>Dairy</label>
+          <label>Dairy</label>
           
           <input value='Gluten' name='allergens' onChange={handleChange} id='Gluten' type='checkbox'/>
-          <label for='Gluten'>Gluten(Wheat, Rye, Spelt, Oats, Kamut)</label>
+          <label>Gluten(Wheat, Rye, Spelt, Oats, Kamut)</label>
           
           <input value='Eggs' name='allergens' onChange={handleChange} id='Eggs' type='checkbox'/>
-          <label for='Eggs'>Eggs</label>
+          <label>Eggs</label>
           
           <input value='Fish' name='allergens' onChange={handleChange} id='Fish' type='checkbox'/>
-          <label for='Fish'>Fish</label>
+          <label>Fish</label>
 
           <input value='Shellfish' name='allergens' onChange={handleChange} id='Shellfish' type='checkbox'/>
-          <label for='Shellfish'>Shellfish</label>
+          <label>Shellfish</label>
 
           <input value='Peanuts' name='allergens' onChange={handleChange} id='Peanuts' type='checkbox'/>
-          <label for='Peanuts'>Peanuts</label>
+          <label>Peanuts</label>
 
           <input value='Sesame' name='allergens' onChange={handleChange} id='Sesame' type='checkbox'/>
-          <label htmlfor='Sesame'>Sesame</label>
+          <label>Sesame</label>
 
           <input value='Lupin' name='allergens' onChange={handleChange} id='Lupin' type='checkbox'/>
-          <label for='Lupin'>Lupin</label>
+          <label>Lupin</label>
 
           <input value='Nuts' name='allergens' onChange={handleChange} id='Nuts' type='checkbox'/>
-          <label for='Nuts'>Nuts</label>
+          <label>Nuts</label>
 
           <input value='Soy' name='allergens' onChange={handleChange} id='Soy' type='checkbox'/>
-          <label for='Soy'>Soy</label>
+          <label>Soy</label>
 
           <input value='Milk' name='allergens' onChange={handleChange} id='Milk' type='checkbox'/>
-          <label for='Milk'>Milk</label>
+          <label>Milk</label>
         </div>
         <textarea 
           name="storageInformation"
